0b1257dd9b357d3ff11bb7751f9da499f3ea6895
Husain Folath <Hussain.folath@gmail.com>
2025-08-17 13:21:56 +0300

    feat: made the basic structure of the description page
    Put into account that when it is called that you pass the movie object alongside loading and error

--- src/pages/Description/Description.jsx
+++ src/pages/Description/Description.jsx
@@ -1,7 +1,23 @@
 
 // pages/Description/Description.jsx
 import { useParams } from "react-router-dom";
-export default function Description(){
+export default function Description({ movie, loading, error }) {
   const { id } = useParams();
-  return <h1>Description for {id}</h1>;
+  if (loading) return <p className="muted">Loading…</p>;
+  if (error) return <p className="error">{error}</p>;
+  if (!movie) return <p className="muted">Search for a movie to begin.</p>;
+
+  return (
+    <div>
+
+      <h1>Description for {id}</h1>
+      <h2>{movie.Title}</h2>
+      <p>{movie.Genre}</p>
+      <p> <strong>Released:</strong> {movie.Released}</p>
+      <p><strong>Rated:</strong> {movie.Rated}  • <strong> Metascore:</strong> {movie.Metascore} • <strong> IMDb rating:</strong>  {movie.imbdRating}</p>
+      <p><strong>Runtime:</strong> {movie.Runtime}</p>
+      <p>Earnings: {movie.BoxOffice}</p>
+      <p>{movie.Plot}</p>
+    </div>
+  )
 }
